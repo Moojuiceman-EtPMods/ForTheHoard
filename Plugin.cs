1ce58b5417c83fb20eafeb900dbba7a1fc352629
Alex Barrett <moojuiceman@hotmail.co.uk>
2023-01-19 19:37:52 +0000

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,72 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ForTheHoard
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        static ManualLogSource logger;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            logger = Logger;
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded");
+            Logger.LogInfo($"Patching...");
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+            Logger.LogInfo($"Patched");
+        }
+
+        [HarmonyPatch(typeof(TrunkHolder), "Start")]
+        [HarmonyPostfix]
+        static void Start_Postfix(TrunkHolder __instance)
+        {
+            __instance.minDetectDistance = 0.1f;
+            __instance.minDetectDistanceAttached = 0.1f;
+        }
+
+        // Debug drawing
+        /*
+        static GameObject lines;
+
+        [HarmonyPatch(typeof(TrunkHolder), "Start")]
+        [HarmonyPostfix]
+        static void Start_Postfix(TrunkHolder __instance)
+        {
+            foreach (TrunkHolder.detectPoint_t t in __instance.detectPoints)
+            {
+                GameObject g = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                g.transform.SetParent(t.detectPoint.transform);
+                g.transform.localPosition = new Vector3(0, 0, 0);
+                g.transform.localScale = new Vector3(.02f, .02f, .02f);
+            }
+
+            lines = new GameObject();
+            lines.transform.SetParent(__instance.transform);
+        }
+
+        [HarmonyPatch(typeof(TrunkHolder), "GetAddIndex")]
+        [HarmonyPrefix]
+        static void GetAddIndex_Prefix(TrunkHolder __instance)
+        {
+            UnityEngine.Object.Destroy(lines);
+            lines = new GameObject();
+            lines.transform.SetParent(__instance.transform);
+        }
+
+        [HarmonyPatch(typeof(TrunkHolder), "GetDetectDistance")]
+        [HarmonyPostfix]
+        static void GetDetectDistance_Postfix(TrunkHolder __instance, Vector3 detectPos)
+        {
+            GameObject g = new GameObject();
+            g.transform.SetParent(lines.transform);
+            LineRenderer l = g.AddComponent<LineRenderer>();
+            l.SetWidth(.1f, .1f);
+            l.SetPositions(new Vector3[] { detectPos, detectPos + (-__instance.transform.up * __instance.maxDetectDistance) });
+        }
+        */
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,28 @@
+# ForTheHoard
+
+This is a mod for the game Escape The Pacific by Gamers4Gamers Team. You can get it here: https://store.steampowered.com/app/655290/Escape_The_Pacific/
+
+#### What it does
+
+Lets you store more in item holders.
+
+#### Save file backup
+
+This shouldn't break saves, but taking backups is recommended.
+
+* Saves are located at C:\Users\\\<Username>\AppData\LocalLow\Gamers4Gamers Team\Escape The Pacific\SavesDir
+* Make a copy of the folder, either to another location or as a new folder name in the same place
+* If sharing the save, zip the folder for easier transfer
+
+#### Installation
+
+* Download BepInEx 5 64-bit. Here is a direct link to version 5.4.21 which works with the game https://github.com/BepInEx/BepInEx/releases/download/v5.4.21/BepInEx_x64_5.4.21.0.zip
+* Unzip it into the game folder, next to EscapeThePacific.exe. Find it via Steam by right clicking the game > Manage > Browse local files
+* Start the game once so BepInEx creates the necessary folders
+* Grab the latest .dll from the releases page and put it in the BepInEx\plugins folder
+ 
+#### Configuration
+
+Mod options are stored in BepInEx\config\\\<Modname>.cfg
+
+This mod has no configurable options.
